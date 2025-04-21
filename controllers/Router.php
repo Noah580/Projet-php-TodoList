f7a50c9ebc67f2160dbf03b8d76a3e5b89cdec61
Noah <Noah580@users.noreply.github.com>
2025-04-21 12:13:54 +0200

    Premier ajout du projet semaine 2 TodoList crud en php

---
+++ controllers/Router.php
@@ -0,0 +1,9 @@
+<?php
+  function displayHome(){
+    require __DIR__ . '/../public/views/Welcome.php';
+  }
+
+  function Display404(){
+    require __DIR__ . "/../public/views/404.php";
+  }
+?>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!-- Fichier pour les routes -->
+
+<?php 
+  require "./controllers/Router.php";
+
+  if (isset($_GET["page"]) && !empty($_GET["page"])) {
+    $page = htmlspecialchars($_GET["page"]);
+
+    if ($page == "home") {
+      displayHome();
+    }else{
+      Display404();
+    }
+  }
+
+?>
